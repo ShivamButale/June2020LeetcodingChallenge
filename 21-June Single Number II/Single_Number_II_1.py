0be88477a18b580d2113cd9f27e2c8ae3190a669
Shivam Butale <shivbutale22@gmail.com>
2020-06-23 15:21:24 +0530

    Delete Single_Number_II_1.py

--- 21-June Single Number II/Single_Number_II_1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        msp = defaultdict(int)
-        for i in nums:
-            msp[i] = nums.count(i)
-            if msp[i] == 1:
-                return i
